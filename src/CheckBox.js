ac1429416bf42440db9f513d7a1667f2435b24d7
eunjae <zhalr103@naver.com>
2022-04-29 13:22:30 +0900

    css추가

--- src/CheckBox.js
+++ src/CheckBox.js
@@ -4,8 +4,8 @@ import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 const CheckBox = () => {
   const [fruits, setFruits] = useState([
-    { id: 1, text: "사과", active: true },
-    { id: 2, text: "바나나", active: true },
+    { id: 1, text: "사과", active: false },
+    { id: 2, text: "바나나", active: false },
     { id: 3, text: "배", active: false },
     { id: 4, text: "딸기", active: false },
   ]);
@@ -22,6 +22,7 @@ const CheckBox = () => {
       <Box>
         <Insert>
           <input type="text" placeholder="좋아하는 과일을 입력하세요" />
+          <button type="button">Add</button>
         </Insert>
         {fruits.map((item) => (
           <Check key={item.id} onClick={() => onToggle(item.id)}>
@@ -35,12 +36,40 @@ const CheckBox = () => {
 };
 
 const Insert = styled.div`
-  border: 1px solid red;
-  height: 50px;
+  height: 60px;
   display: flex;
-  flex: 1;
+  padding: 20px;
+  background: #ffb88c;
   input {
+    border: none;
+    background: none;
+    outline: none;
     border: 1px solid #333;
+    font-size: 18px;
+    padding: 10px;
+    margin-right: 10px;
+    background: #fff;
+    ::placeholder {
+      font-size: 15px;
+    }
+  }
+  button {
+    border: none;
+    background: none;
+    outline: none;
+    width: 70px;
+    border: 1px solid #666;
+    background: #fff;
+    font-size: 16px;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    color: #333;
+    transition: 0.3s ease-in;
+    :hover {
+      background: #999;
+      color: #fff;
+    }
   }
 `;
 const Box = styled.div`
@@ -55,10 +84,19 @@ const Check = styled.div`
   :first-child {
     border-top: none;
   }
+  :nth-child(even) {
+    background: #79cbca;
+  }
   font-size: 18px;
   display: flex;
   align-items: center;
   text-indent: 10px;
+  color: #333;
+  background: #ffedbc;
+  :hover {
+    color: #de6262;
+    font-weight: bold;
+  }
   svg {
     margin-left: 10px;
   }
